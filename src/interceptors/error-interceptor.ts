7f2de3bb90d7489780f981addb4ca500e91ecfae
Heitor Bernardino <heitor.albano1984@gmail.com>
2020-02-06 18:02:39 -0300

    Tratamento padrão para outros erros com Alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -2,12 +2,14 @@ import { StorageService } from './../services/storage.service';
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx'; // IMPORTANTE: IMPORT ATUALIZADO
+import { AlertController } from 'ionic-angular';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
   constructor(
-    public storage: StorageService
+    public storage: StorageService,
+    public alertController: AlertController
   ){}
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -28,9 +30,14 @@ export class ErrorInterceptor implements HttpInterceptor {
           console.log(errorObj);
 
           switch(errorObj.status) {
+            case 401:
+              this.handle401();
+              break;
             case 403:
               this.handle403();
               break;
+            default:
+              this.handleDefaultError(errorObj)
           }
 
           return Observable.throw(errorObj);
@@ -39,6 +46,36 @@ export class ErrorInterceptor implements HttpInterceptor {
 
     handle403() {
       this.storage.setLocalUser(null);
+    }
+
+    handle401() {
+      let alert = this.alertController.create({
+        title: 'Erro 401: Falha de autenticação',
+        message: 'Email ou senha incorretos',
+        enableBackdropDismiss: false,
+        buttons: [
+          {
+            text: 'OK'
+          }
+        ]
+      });
+
+      alert.present();
+    }
+
+    handleDefaultError(errorObj) {
+      let alert = this.alertController.create({
+        title: `Erro ${errorObj.status}: ${errorObj.error}`,
+        message: errorObj.message,
+        enableBackdropDismiss: false,
+        buttons: [
+          {
+            text: 'OK'
+          }
+        ]
+      });
+
+      alert.present();
     }
   }
 
